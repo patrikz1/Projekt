154d6e4266f6e0f1a0388f87d710a7a5782d9b55
Chrillchrill <christian--_@hotmail.com>
2018-11-01 12:09:24 +0100

    update

--- Projekt/Serializer.cs
+++ Projekt/Serializer.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Projekt
 {
-    class Serializer
-    {   
+    namespace Data
+    {
+        public class DataSerializer
+        {
+            string filepath;
+
+
+            public DataSerializer()
+            {
+                var folder = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                filepath = System.IO.Path.Combine(folder, "Feeds");
+            }
+
+            public void SaveToFile(List<Tillvårfeed> feed)
+            {
+                List<klassenSomTarEmotItems> Items = feed.Select(x => new SerializerItem(x)).ToList();
+                var xml = new System.Xml.Serialization.XmlSerializer(typeof(List<SerializerItem>));
+                using (var f = System.IO.File.Open(filepath, System.IO.FileMode.Create))
+                {
+
+                    xml.Serialize(f, Items);
+                    f.Flush();
+                    f.Close();
+                }
+            }
+
+            public delegate void Del(SerializerItem returFeed);
+
+
+            public void LoadFromFile(Del Callback)
+            {
+                if (System.IO.File.Exists(filepath))
+                {
+                    List<SerializerItem> returFeed;
+
+                    var xml = new System.Xml.Serialization.XmlSerializer(typeof(List<SerializerItem>));
+                    using (var s = System.IO.File.Open(filepath, System.IO.FileMode.Open))
+                    {
+                        returFeed = xml.Deserialize(s) as List<SerializerItem>;
+                        s.Flush();
+                        s.Close();
+                    }
+
+                    returFeed.ForEach(x => Callback(x));
+
+                }
+            }
+
+        }
     }
-}
+
+
